0686feeac830240b46b5b72e71e790e9a09616fe
Thane <mtfenton1991@gmail.com>
2016-07-20 23:50:12 -0700

    Added SharedPreferences to save data

--- HabitFormer/app/src/main/java/com/saymtf/habitformer/Habit.java
+++ HabitFormer/app/src/main/java/com/saymtf/habitformer/Habit.java
@@ -1,14 +1,18 @@
 package com.saymtf.habitformer;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class Habit extends AppCompatActivity {
-
+    public static final String PUBLIC_STATIC_STRING_IDENTIFIER = "com.saymtf.habit.HABITNAME";
+    private EditText habitNameText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +29,21 @@ public class Habit extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        habitNameText = (EditText) findViewById(R.id.habit_name);
     }
 
+    public void createTheHabit(View view) {
+        String habitName = habitNameText.getText().toString();
+        if(!habitName.equals("")) {
+            Intent resultIntent = new Intent();
+            resultIntent.putExtra(PUBLIC_STATIC_STRING_IDENTIFIER, habitName);
+            setResult(Activity.RESULT_OK, resultIntent);
+            finish();
+        }else {
+            // Display message, Cannot be Empty
+            System.out.println("Empty");
+
+        }
+    }
 }
--- HabitFormer/app/src/main/java/com/saymtf/habitformer/MainActivity.java
+++ HabitFormer/app/src/main/java/com/saymtf/habitformer/MainActivity.java
@@ -1,6 +1,9 @@
 package com.saymtf.habitformer;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,9 +12,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int STATIC_INTEGER_VALUE = 69;
+    public static final String PUBLIC_STATIC_STRING_IDENTIFIER = "com.saymtf.habit.HABITNAME";
+    public static final String HABIT_NAME = "com.saymtf.habit.A_HABIT_NAME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +35,17 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        SharedPreferences sharedPref = getApplicationContext().getSharedPreferences(HABIT_NAME, 0);
+        String habitName = sharedPref.getString("habitName", HABIT_NAME);
+        if (!habitName.equals(HABIT_NAME)) {
+            TextView habitNameTextView = new TextView(this);
+            habitNameTextView.setTextSize(50);
+            habitNameTextView.setText(habitName);
+
+            RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+            layout.addView(habitNameTextView);
+        }
     }
 
     @Override
@@ -52,9 +70,37 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == STATIC_INTEGER_VALUE) {
+            if(resultCode == Activity.RESULT_OK) {
+                String habit = data.getStringExtra(PUBLIC_STATIC_STRING_IDENTIFIER);
+                // Update View
+                TextView habitTextView = new TextView(this);
+                habitTextView.setTextSize(40);
+                habitTextView.setText(habit);
+
+                RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
+                layout.addView(habitTextView);
+
+                // Save The Information using
+                // https://developer.android.com/training/basics/data-storage/shared-preferences.html
+                SharedPreferences sharedPref = getApplicationContext().getSharedPreferences(HABIT_NAME, 0);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("habitName", habitTextView.getText().toString());
+                editor.apply();
+
+            }
+        }
+
+    }
+
     public void addHabit(View view) {
         Intent intent = new Intent(this, Habit.class);
-        startActivity(intent);
+        startActivityForResult(intent, STATIC_INTEGER_VALUE);
     }
 
 }
