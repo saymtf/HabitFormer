86826642d4406e0e90cd4791b6d5065845cd365b
Thane <mtfenton1991@gmail.com>
2016-09-19 22:25:00 -0700

    Backup

--- HabitFormer/app/src/main/java/com/saymtf/habitformer/ConfigureTime.java
+++ HabitFormer/app/src/main/java/com/saymtf/habitformer/ConfigureTime.java
@@ -21,18 +21,6 @@ public class ConfigureTime extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_configure_time);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
         habitTime = intent.getIntExtra(MainActivity.HABIT_TIME, 0);
